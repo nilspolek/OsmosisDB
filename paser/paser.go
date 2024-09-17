91d0e68fae7a7894a92d2ea10ba8d3f9c037a313
Witzler <chrisavril24@gmail.com>
2024-09-17 21:23:30 +0200

    Parser beginning

--- paser/paser.go
+++ paser/paser.go
@@ -1,9 +1,17 @@
 package paser
 
+import (
+	"bytes"
+	"errors"
+)
+
 type Paser struct{}
 
 type Command struct {
-	Type string
+	Type     string
+	Keyword  string
+	DataType byte
+	Data     []byte
 }
 
 const (
@@ -23,6 +31,47 @@ func NewPaser() *Paser {
 	return &Paser{}
 }
 
-func (p *Paser) Parse() Command {
+func (p *Paser) Parse(data []byte) (Command, error) {
 	// Parse the input
+
+	command := new(Command)
+
+	var err error
+	command.Type, err = checkCommand(data[:3])
+	if err != nil {
+		return *command, err
+	}
+
+	//payload := strings.Split(string(data[2:]), "\r\n") //"SETNAME\r\n@NILS\n"
+
+	idx := bytes.Index(data, []byte("\r\n"))
+
+	command.Keyword = string(data[3:idx])
+
+	command.DataType = data[idx+2]
+
+	command.Data = data[idx+3:]
+
+	return *command, nil
+}
+
+func checkCommand(test []byte) (string, error) { //(p *Paser) ?
+
+	switch string(test) {
+	case SET:
+		return SET, nil
+	case GET:
+		return GET, nil
+	case DEL:
+		return DEL, nil
+	case UPT:
+		return UPT, nil
+	case ERR:
+		return ERR, nil
+	case OK:
+		return OK, nil
+	default:
+		return "", errors.New("Command type not found")
+	}
+
 }
--- paser/paser_test.go
+++ paser/paser_test.go
@@ -1,11 +1,32 @@
 package paser
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestParse(t *testing.T) {
-	input := "SETNAME\r\nNILS\n"
+	input := "SETNAME\r\n@NILS\n"
 	paser := NewPaser()
-	paser.Parse()
+	output, err := paser.Parse([]byte(input))
+
+	fmt.Println(output.Type)
+	fmt.Println(output.Keyword)
+	fmt.Println(string(output.DataType))
+	fmt.Println(string(output.Data))
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	if output.Type != SET {
+		t.Fatal("Expected Command: SET")
+	}
+
+	input = "SkTNAME\r\n@NILS\n"
+	output, err = paser.Parse([]byte(input))
+
+	if err == nil {
+		t.Fatal("Expected error")
+	}
+
 }
