f9b927d7818b83895e85046434631b67ace4dd2d
Nils Polek <nils@polek.de>
2024-09-18 16:49:34 +0200

    Added goLog

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/nilspolek/OsmosisDB
 
-go 1.20
+go 1.23rc2
+
+toolchain go1.23.1
+
+require github.com/nilspolek/goLog v1.0.5 // indirect
--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"github.com/nilspolek/OsmosisDB/server"
+	"github.com/nilspolek/goLog"
 )
 
 func main() {
 	server := server.NewServer(server.NewServerConfig(":8080"))
+	goLog.Info("Starting server on port 8080")
 	server.Start()
 	defer server.Stop()
 
