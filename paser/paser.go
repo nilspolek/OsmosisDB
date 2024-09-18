1bff63611f6fa14601585d9fc67e58276cd61128
Nils Polek <nils@polek.de>
2024-09-18 14:53:54 +0200

    Parser may be compleat

--- paser/paser.go
+++ paser/paser.go
@@ -33,45 +33,78 @@ func NewPaser() *Paser {
 
 func (p *Paser) Parse(data []byte) (Command, error) {
 	// Parse the input
-
-	command := new(Command)
-
-	var err error
-	command.Type, err = checkCommand(data[:3])
-	if err != nil {
-		return *command, err
+	var (
+		err     error
+		command Command
+	)
+	switch string(data[:3]) {
+	case SET:
+		command, err = p.parseSet(data)
+		break
+	case GET:
+		command, err = p.parseGet(data)
+		break
+	case DEL:
+		command, err = p.parseDel(data)
+		break
+	case UPT:
+		command, err = p.parseUpt(data)
+		break
+	case ERR:
+		command, err = Command{Type: ERR}, nil
+		break
+	case OK:
+		command, err = Command{Type: OK}, nil
+		break
+	default:
+		err = errors.New("Command type not found")
+		break
 	}
-
-	//payload := strings.Split(string(data[2:]), "\r\n") //"SETNAME\r\n@NILS\n"
-
-	idx := bytes.Index(data, []byte("\r\n"))
-
-	command.Keyword = string(data[3:idx])
-
-	command.DataType = data[idx+2]
-
-	command.Data = data[idx+3:]
-
-	return *command, nil
+	if err != nil {
+		return Command{}, err
+	}
+	return command, nil
 }
 
-func checkCommand(test []byte) (string, error) { //(p *Paser) ?
+func (p *Paser) parseUpt(data []byte) (Command, error) {
+	var (
+		command Command
+		idx     int
+	)
+	idx = bytes.Index(data, []byte("\r\n"))
+	command.Type = UPT
+	command.Keyword = string(data[3:idx])
+	command.DataType = data[idx+2]
+	command.Data = data[idx+3 : len(data)-1]
+	return command, nil
+}
 
-	switch string(test) {
-	case SET:
-		return SET, nil
-	case GET:
-		return GET, nil
-	case DEL:
-		return DEL, nil
-	case UPT:
-		return UPT, nil
-	case ERR:
-		return ERR, nil
-	case OK:
-		return OK, nil
-	default:
-		return "", errors.New("Command type not found")
-	}
+func (p *Paser) parseDel(data []byte) (Command, error) {
+	var (
+		command Command
+	)
+	command.Type = DEL
+	command.Keyword = string(data[3 : len(data)-1])
+	return command, nil
+}
 
+func (p *Paser) parseSet(data []byte) (Command, error) {
+	var (
+		command Command
+		idx     int
+	)
+	idx = bytes.Index(data, []byte("\r\n"))
+	command.Type = SET
+	command.Keyword = string(data[3:idx])
+	command.DataType = data[idx+2]
+	command.Data = data[idx+3 : len(data)-1]
+	return command, nil
+}
+func (p *Paser) parseGet(data []byte) (Command, error) {
+	var (
+		command Command
+	)
+	command.Type = GET
+	command.Keyword = string(data[3 : len(data)-1])
+	return command, nil
 }
--- paser/paser_test.go
+++ paser/paser_test.go
@@ -30,3 +30,121 @@ func TestParse(t *testing.T) {
 	}
 
 }
+
+func TestGET(t *testing.T) {
+	input := "GETNAME\n"
+	paser := NewPaser()
+	output, err := paser.Parse([]byte(input))
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	if output.Type != GET {
+		t.Fatal("Expected Command: GET")
+	}
+	if output.Keyword != "NAME" {
+		t.Fatalf("Expected Keyword: NAME, got %s", output.Keyword)
+	}
+	if output.DataType != 0 {
+		t.Fatalf("Expected DataType: 0, got %v", output.DataType)
+	}
+	if output.Data != nil {
+		t.Fatal("Expected no Data")
+	}
+}
+
+func TestDEL(t *testing.T) {
+	input := "DELNAME\n"
+	paser := NewPaser()
+	output, err := paser.Parse([]byte(input))
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	if output.Type != DEL {
+		t.Fatal("Expected Command: DEL")
+	}
+	if output.Keyword != "NAME" {
+		t.Fatalf("Expected Keyword: NAME, got %s", output.Keyword)
+	}
+	if output.DataType != 0 {
+		t.Fatalf("Expected DataType: 0, got %v", output.DataType)
+	}
+	if output.Data != nil {
+		t.Fatal("Expected no Data")
+	}
+}
+
+func TestUPT(t *testing.T) {
+	input := "UPTNAME\r\n@NILS\n"
+	paser := NewPaser()
+	output, err := paser.Parse([]byte(input))
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	if output.Type != UPT {
+		t.Fatal("Expected Command: UPT")
+	}
+	if output.Keyword != "NAME" {
+		t.Fatal("Expected Keyword: NAME")
+	}
+	if output.DataType != TYPESTRING {
+		t.Fatalf("Expected DataType: %v", TYPESTRING)
+	}
+	if output.Data == nil {
+		t.Fatal("Expected Data")
+	}
+	if string(output.Data) != "NILS" {
+		t.Fatalf("Expected Data: NILS, got %s", string(output.Data))
+	}
+}
+
+func TestERR(t *testing.T) {
+	input := "ERR\n"
+	paser := NewPaser()
+	output, err := paser.Parse([]byte(input))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if output.Type != ERR {
+		t.Fatal("Expected Command: ERR")
+	}
+}
+
+func TestOK(t *testing.T) {
+	input := "OK \n"
+	paser := NewPaser()
+	output, err := paser.Parse([]byte(input))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if output.Type != OK {
+		t.Fatal("Expected Command: OK ")
+	}
+}
+
+func TestSET(t *testing.T) {
+	input := "SETNAME\r\n@NILS\n"
+	paser := NewPaser()
+	output, err := paser.Parse([]byte(input))
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	if output.Type != SET {
+		t.Fatal("Expected Command: SET")
+	}
+	if output.Keyword != "NAME" {
+		t.Fatal("Expected Keyword: NAME")
+	}
+	if output.DataType != TYPESTRING {
+		t.Fatalf("Expected DataType: %v", TYPESTRING)
+	}
+	if output.Data == nil {
+		t.Fatal("Expected Data")
+	}
+	if string(output.Data) != "NILS" {
+		t.Fatalf("Expected Data: NILS, got %s", string(output.Data))
+	}
+}
