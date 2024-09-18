1fae8c4fc42eff776d5bf772cbeea9ac7e2e4593
Nils Polek <nils@polek.de>
2024-09-18 15:22:35 +0200

    Added basich server

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World")
+	fmt.Println("Listening on port 8080")
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println("Error starting TCP server:", err)
+		os.Exit(1)
+	}
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			continue
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	fmt.Println("Client connected:", conn.RemoteAddr())
+	conn.Write([]byte("Hello from server\n"))
 }
